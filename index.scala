1fa07e5748ab2b3a30a03b7999f4ed4cfcc399c5
Anton Sviridov <keynmol@gmail.com>
2024-05-20 19:32:28 +0100

    Make it a counter

--- index.scala
+++ index.scala
@@ -6,12 +6,21 @@ import org.scalajs.dom.*
 
 @main def hello =
   val container = document.getElementById("content")
+
   val button = document.createElement("button")
   button.innerText = "Click me!"
   container.appendChild(button)
 
-  button.addEventListener(
-    "click",
-    ev => window.alert("Clicked: " + ev.toString())
-  )
+  val count = container.appendChild(document.createElement("p"))
+  container.appendChild(count)
+
+  var counter = 0
+  def setCounter() =
+    counter += 1
+    count.innerText = counter.toString()
+
+  setCounter()
+
+  button.addEventListener("click", ev => setCounter())
+
 end hello
