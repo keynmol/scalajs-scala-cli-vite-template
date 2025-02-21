29a324f53b6a4b6206ac2c7e9c667c5c03f48d31
Anton Sviridov <keynmol@gmail.com>
2025-02-21 11:13:45 +0000

    add devcontainer configuration

--- index.scala
+++ index.scala
@@ -1,8 +1,3 @@
-//> using platform scala-js
-//> using jsVersion 1.18.2
-//> using dep org.scala-js::scalajs-dom::2.8.0
-//> using scala 3.6.3
-
 import org.scalajs.dom.*
 
 @main def hello =
---
+++ project.scala
@@ -0,0 +1,5 @@
+//> using platform scala-js
+//> using jsVersion 1.18.2
+//> using dep org.scala-js::scalajs-dom::2.8.0
+//> using scala 3.6.3
+
